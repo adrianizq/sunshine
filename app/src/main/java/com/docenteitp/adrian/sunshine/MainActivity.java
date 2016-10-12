223acebc7120fc1312d76bccdcaa4424dcf171d6
adrianizq <aizquierdo@itp.edu.co>
2016-10-12 16:38:54 -0500

    Intent Mapa Google

--- app/src/main/java/com/docenteitp/adrian/sunshine/MainActivity.java
+++ app/src/main/java/com/docenteitp/adrian/sunshine/MainActivity.java
@@ -2,6 +2,9 @@ package com.docenteitp.adrian.sunshine;
 
 //import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 
 import android.os.Bundle;
@@ -28,6 +31,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +58,34 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
+        if(id==R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPreferences=
+                PreferenceManager.getDefaultSharedPreferences(
+                        this);
+        String location = sharedPreferences.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+        else{
+            Log.d(LOG_TAG,"No se pudo llamar "+ location +" no existe la app");
+        }
+
     }
 }
